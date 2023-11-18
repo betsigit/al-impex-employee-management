99d0c7745091d83d54e805c5beb598530c9684ed
TsionFitsum <tsifitsum20@gmail.com>
2023-11-18 11:50:54 +0300

    Backend to save

--- src/Pages/Admin/InterviewForm.js
+++ src/Pages/Admin/InterviewForm.js
@@ -1,7 +1,7 @@
+
 import React, { useState } from 'react';
 import Header from '../Component/Header'
 import { AiOutlineClose } from 'react-icons/ai'
-
 
 
 const InterviewForm = ({ handleModalClose }) => {
@@ -11,17 +11,16 @@ const InterviewForm = ({ handleModalClose }) => {
   const [phoneNo, setphoneNo] = useState([]);
   const [email, setEmail] = useState([]);
   const [department, setDepartment] = useState([]);
-
-
+  const [User, setUser] = useState([]);
 
   const [selectedFile, setSelectedFile] = useState(null);
 
+  // const [questions, setQuestions] = useState([{ question: '', answer: '' }]);
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
     console.log(event.target.files[0]);
   }
-
   const handleUpload = () => {
     if (selectedFile) {
       // Perform file upload logic here (e.g., send the file to the server)
@@ -32,76 +31,96 @@ const InterviewForm = ({ handleModalClose }) => {
       alert('Please select a file before uploading.');
     }
   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  const handleQuestionChange = (index, type, value) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index][type] = value;
+    setQuestions(updatedQuestions);
+  };
 
   const handleAddQuestion = () => {
     setQuestions([...questions, { question: '', answer: '' }]);
   };
 
-  const handleInputChange = (index, type, value) => {
-    const updatedQuestions = [...questions];
-    updatedQuestions[index][type] = value;
-    setQuestions(updatedQuestions);
-  };
-  const handleSubmit = () => {
-    if (questions.length === 0) {
-      alert("Nothing to submit");
-    } else {
-      const url = "http://localhost/enquiry.php";
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-      let fData = new FormData();
-      fData.append('Fullname', name);
-      fData.append('phoneNo', phoneNo);
-      fData.append('questions', questions);
+    const questionsData = questions.map((q) => ({
+      question: q.question,
+      answer: q.answer,
+      
+    }));
+    
+    console.log(questionsData);
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          Email: email,
+          Phoneno: phoneNo,
+          Department: department,
+          questions: questionsData,
+        }),
+      };
 
-      /*axios.post(url,fData)
-      .then(response=> alert(response.data))
-      .catch(error=> alert(error));*/
+      const response = await fetch('https://al-impex-default-rtdb.firebaseio.com/Al-Impex.json', options);
+
+      if (response.ok) {
+        alert('Message sent');
+        // Additional logic after successful submission if needed
+      } else {
+        alert('Error occurred');
+      }
+    } catch (error) {
+      console.error('Error submitting data:', error);
     }
   };
-  const onSubmit = (data) => {
-    console.log(data);
-  };
 
-  /*const handleReset = () => {
-    setName('');
-    setphoneNo('');
-    setEmail('');
-    setQuestions(['']);
-  };*/
   return (
     <div className=" bg-half-transparent  fixed inset-0  flex justify-center items-center overflow-y-auto">
-      <div className="float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] max-w-screen w-full sm:w-full md:w-full lg:w-full xl:w-1/2 2xl:w-1/3 overflow-y-auto rounded-lg" style={{ width: "70%", height: "90%" }}>
+     < div className="float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] max-w-screen w-full sm:w-full md:w-full lg:w-full xl:w-1/2 2xl:w-1/3 overflow-y-auto rounded-lg" style={{ width: "70%", height: "90%" }}>
 
-        <div className="w-full p-6">
-          <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+<div className="w-full p-6">
+  <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
 
-            <Header category="Employement" title="INTERVIEW Q & A" />
+    <Header category="Employement" title="INTERVIEW Q & A" />
 
-            <button
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              onClick={handleModalClose}
-              data-modal-hide="editUserModal"
-            >
-              <AiOutlineClose className="w-5 h-5" />
-            </button>
-          </div>
-          <h1 className="text-3xl font-semibold text-center text-green-700 underline uppercase ">
+    <button
+      type="button"
+      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+      onClick={handleModalClose}
+      data-modal-hide="editUserModal"
+    >
+      <AiOutlineClose className="w-5 h-5" />
+    </button>
+  </div>
+  <h1 className="text-3xl font-semibold text-center text-green-700 underline uppercase ">
 
-          </h1>
-          <div className="mb-2">
-            <label
-              for="uploadCV"
-              className="block text-sm font-semibold text-gray-800">Upload CV
-            </label>
+  </h1>
+  <div className="mb-2">
+    <label
+      for="uploadCV"
+      className="block text-sm font-semibold text-gray-800">Upload CV
+    </label>
 
-            <input
-              type="file"
-              accept=".pdf, .doc, .docx, .jpg, .png"
-              onChange={handleFileChange}
-            />
-            <button onClick={handleUpload}>Upload File</button>
-            <label
+
+<input
+      type="file"
+      accept=".pdf, .doc, .docx, .jpg, .png"
+      onChange={handleFileChange}
+    />
+    <button onClick={handleUpload}>Upload File</button>
+     
+        {/* User Info Fields */}
+        <label
               for="fullname"
               className="block text-sm font-semibold text-gray-800"
             >
@@ -115,6 +134,7 @@ const InterviewForm = ({ handleModalClose }) => {
               value={name} onChange={(e) => setName(e.target.value)}
             />
           </div>
+
           <div className="mb-2">
             <label
               for="email"
@@ -130,6 +150,7 @@ const InterviewForm = ({ handleModalClose }) => {
               value={email} onChange={(e) => setEmail(e.target.value)}
             />
           </div>
+
           <div className="mb-2">
             <label
               for="phoneNo"
@@ -160,49 +181,46 @@ const InterviewForm = ({ handleModalClose }) => {
               value={department} onChange={(e) => setDepartment(e.target.value)}
             />
           </div>
-          {questions.map((q, index) => (
-            <div key={index} className="mb-4">
-              <label className="block text-sm font-semibold mb-2">
-                Question {index + 1}:
-              </label>
-              <input
+
+        {/* Dynamic Question and Answer Fields */}
+        {questions.map((q, index) => (
+          <div key={index} className="mb-4">
+            <label className="block text-sm font-semibold mb-2">
+              Question {index + 1}:
+              <input className="w-full p-2 border rounded"
                 type="text"
                 value={q.question}
-                onChange={(e) => handleInputChange(index, 'question', e.target.value)}
-                className="w-full p-2 border rounded"
+                onChange={(e) => handleQuestionChange(index, 'question', e.target.value)
+              }
               />
-              <label className="block text-sm font-semibold mt-2 mb-2">
-                Answer {index + 1}:
-              </label>
-              <input
+            </label>
+            <label className="block text-sm font-semibold mt-2 mb-2">
+              Answer {index + 1}:
+              <input  className="w-full p-2 border rounded"
                 type="text"
                 value={q.answer}
-                onChange={(e) => handleInputChange(index, 'answer', e.target.value)}
-                className="w-full p-2 border rounded"
+                onChange={(e) => handleQuestionChange(index, 'answer', e.target.value)}
               />
-            </div>
-
-          ))}
+            </label>
+          </div>
+        ))}
           <div class="flex justify-between">
-            <button
-              onClick={handleAddQuestion}
-              className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500"
-            >
-              Add More Question
-            </button>
+                    <button type="button" onClick={handleAddQuestion} 
+                    className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500">
+          Add More Question
+        </button>
 
-            <button
-              onClick={onSubmit}
-              className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500"
-            >
-              Submit
-            </button>
+        {/* Submit Button */}
+        <button type="submit" onClick={handleSubmit}
+        className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500">
+          Submit
+        </button>
           </div>
 
-        </div>
+          </div>
       </div>
-    </div>
+      </div>
   );
 };
 
-export default InterviewForm;
+export default InterviewForm ;
